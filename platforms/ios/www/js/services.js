5ff8ba9d75297ae0fb7b76e0ff5dc7d73cdaafc0
sk01 <saurbh1@gmail.com>
2014-08-30 20:12:13 +0100

    some stuff working

--- platforms/ios/www/js/services.js
+++ platforms/ios/www/js/services.js
@@ -3,24 +3,14 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Report', function($http) {
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
 
   return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+    incident: function(reportData){
+      $http.post('http://localhost:3000/incident/report')
     }
   }
 });
+
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,25 +8,18 @@ angular.module('starter.controllers', [])
 
 	$scope.reportIncident = function(data){
 
-		console.lg('reporting', data);
+		console.log('reporting', data);
 
-var alertPopup = $ionicPopup.alert({
-     title: 'Incident reported successfully!',
-     template: 'It might taste good'
-   });
-   alertPopup.then(function(res) {
-     console.log('Thank you for not eating my delicious ice cream cone');
-   });
- };
-		// here you add loc to data
-		//Report.incident(data).then(function(res){
-			//here comes the success
-		//}, function(error){
-			//here comes the error
-		//})
-	})
+		var alertPopup = $ionicPopup.alert({
+	     	title: 'Incident reported successfully!'
+	   	});
 
+	    alertPopup.then(function(res) {
+	    	
+	   	});
 
+    }	
+})
 
 .controller('InfoCtrl', function($scope) {
 })
