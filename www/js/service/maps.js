262dd3b1db5a61a73655f861ffb04f4c952f6502
Carlos Pastor <carlos.pastor@mailonline.co.uk>
2014-08-31 00:57:37 +0100

    Fixed initialization of map.

--- www/js/service/maps.js
+++ www/js/service/maps.js
@@ -1,4 +1,4 @@
-mapStyle = [
+var mapStyle = [
     {
         "featureType": "water",
         "elementType": "geometry",
@@ -293,15 +293,14 @@ angular.module('starter.services')
     .factory('Maps', function ($q, $http) {
 
         var deferredInit = $q.defer();
-        var currentLoc;
+        var currentLoc, myOptions, incidentLocation;
 
-        var getMapWithGPS = function() {
+        var getMapWithGPS = function () {
             navigator.geolocation.getCurrentPosition(onGPSSuccess, onGPSError);
             return deferredInit.promise;
         };
 
-        var onGPSSuccess = function(position) {
-            var incidentLocation, map, marker, myOptions;
+        var onGPSSuccess = function (position) {
             incidentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
             myOptions = {
                 zoom: 16,
@@ -310,52 +309,45 @@ angular.module('starter.services')
                 styles: mapStyle,
                 disableDefaultUI: true
             };
-            map = new google.maps.Map(document.getElementById("map"), myOptions);
-            marker = new google.maps.Marker({
-                position: incidentLocation,
-                draggable: true,
-                map: map
-            });
 
-            google.maps.event.addListener(marker, 'dragend', function(evt) {
-                currentLoc = {
-                  lat: evt.latLng.lat(),
-                  lng: evt.latLng.lng()
-                };
+            currentLoc = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
 
-                deferredInit.resolve(loc);
-            });
+            deferredInit.resolve(currentLoc);
 
         };
 
-        var onGPSError = function(error) {
+        var onGPSError = function (error) {
             deferredInit.reject('code: ' + error.code + '\n' + 'message: ' + error.message + '\n', error);
         };
 
 
-        function getHeatMapResults(){
-            return $http.get('http://localhost:3000/heatmap', {params:currentLoc})
-                    .then(function(res){
-                        return res.data
-                    });
+        function getHeatMapResults() {
+            return $http.get('http://localhost:3000/heatmap', {params: currentLoc})
+                .then(function (res) {
+                    return res.data.result
+                });
         }
 
-        function processIncidentData(res){
+        function processIncidentData(res) {
             var incidentData = [];
-            angular.forEach(res, function(val, key){
+            angular.forEach(res, function (val) {
                 incidentData.push(new google.maps.LatLng(val.lat, val.lng));
             });
             return incidentData;
-        }       
+        }
 
-        function publishResultsIntoMap(incidentData){
-              var pointArray = new google.maps.MVCArray(incidentData);
- 
-              heatmap = new google.maps.visualization.HeatmapLayer({
+        function publishResultsIntoMap(incidentData) {
+            var map = new google.maps.Map(document.getElementById("map"), myOptions);
+            var pointArray = new google.maps.MVCArray(incidentData);
+
+            var heatmap = new google.maps.visualization.HeatmapLayer({
                 data: pointArray
-              });
+            });
 
-              heatmap.setMap(map);
+            heatmap.setMap(map);
         }
 
         return {
@@ -363,12 +355,10 @@ angular.module('starter.services')
                 getMapWithGPS()
                     .then(getHeatMapResults)
                     .then(processIncidentData)
-                    .then(publishResultsIntoMap);
-            },
-            getCurrentLoc: function(){
-                var defer = $q.defer();
-                defer.resolve(currentLoc);
-                return defer.promise;
+                    .then(publishResultsIntoMap)
+                    .then(function () {
+                        console.log('MAP INITIALIZED');
+                    });
             }
         };
     });
