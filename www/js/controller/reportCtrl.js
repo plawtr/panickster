12c00a910c490da0571db08f01dac1b9058804aa
sk01 <saurbh1@gmail.com>
2014-08-30 22:45:07 +0100

    reporting working a ok

--- www/js/controller/reportCtrl.js
+++ www/js/controller/reportCtrl.js
@@ -1,8 +1,17 @@
+var position;
+
 angular.module('starter.controllers')
     .controller('ReportCtrl', function ($scope, Report, $ionicPopup, $state) {
+
         $scope.data = {};
 
+        navigator.geolocation.getCurrentPosition(getPosition, noCoords);
+
         $scope.reportIncident = function (data) {
+
+            data.lat = position.coords.latitude;
+            data.lng = position.coords.longitude;
+            
             Report.incident(data).then(function (res) {
                 var alertPopup = $ionicPopup.alert({
                     title: 'Incident reported successfully!'
@@ -12,7 +21,13 @@ angular.module('starter.controllers')
                     $state.go('tab.heatmap');
                 });
             });
-
-
         }
     });
+
+getPosition = function(pos) {
+        position = pos;
+}
+
+noCoords = function(error) { 
+    return alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+}
--- www/js/service/report.js
+++ www/js/service/report.js
@@ -1,12 +1,9 @@
 angular.module('starter.services')
 
-/**
- * A simple example service that returns some data.
- */
 .factory('Report', function($http) {
   return {
     incident: function(data){
-      return $http.post('http://localhost:3000/incidents/report', data).then(function(res){
+      return $http.post('http://localhost:3000/incidents/report.json', data).then(function(res){
         return res.data;
       });
     }
