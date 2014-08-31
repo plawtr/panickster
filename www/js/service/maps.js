2ffb4ea1d542d7aa29a888d223713219eed90cf9
Carlos Pastor <carlos.pastor@mailonline.co.uk>
2014-08-31 03:00:02 +0100

    Making the map blue

--- www/js/service/maps.js
+++ www/js/service/maps.js
@@ -1,44 +1,20 @@
 var mapStyle = [
-    {"featureType": "landscape", "stylers": [
-        {"saturation": -100},
-        {"lightness": 65},
-        {"visibility": "on"}
+    {"featureType": "all", "stylers": [
+        {"saturation": 0},
+        {"hue": "#e7ecf0"}
     ]},
-    {"featureType": "poi", "stylers": [
-        {"saturation": -100},
-        {"lightness": 51},
-        {"visibility": "simplified"}
-    ]},
-    {"featureType": "road.highway", "stylers": [
-        {"saturation": -100},
-        {"visibility": "simplified"}
-    ]},
-    {"featureType": "road.arterial", "stylers": [
-        {"saturation": -100},
-        {"lightness": 30},
-        {"visibility": "on"}
-    ]},
-    {"featureType": "road.local", "stylers": [
-        {"saturation": -100},
-        {"lightness": 40},
-        {"visibility": "on"}
+    {"featureType": "road", "stylers": [
+        {"saturation": -70}
     ]},
     {"featureType": "transit", "stylers": [
-        {"saturation": -100},
-        {"visibility": "simplified"}
-    ]},
-    {"featureType": "administrative.province", "stylers": [
         {"visibility": "off"}
     ]},
-    {"featureType": "water", "elementType": "labels", "stylers": [
-        {"visibility": "on"},
-        {"lightness": -25},
-        {"saturation": -100}
+    {"featureType": "poi", "stylers": [
+        {"visibility": "off"}
     ]},
-    {"featureType": "water", "elementType": "geometry", "stylers": [
-        {"hue": "#ffff00"},
-        {"lightness": -25},
-        {"saturation": -97}
+    {"featureType": "water", "stylers": [
+        {"visibility": "simplified"},
+        {"saturation": -60}
     ]}
 ];
 
